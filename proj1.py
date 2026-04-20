9e52bcdf2eb4b73ff1b5560874b57e63798bb795
smf38 <smitch38@calpoly.edu>
2026-04-20 14:57:23 -0700

    finished task 2

--- proj1.py
+++ proj1.py
@@ -25,3 +25,29 @@ class RegionCondition(frozen=True):
 
 #Task 2
 
+region_conditions = [RegionCondition(
+                        Region(
+                            GlobeRect(
+                                37.43, 37.70, 126.77, 127.18
+                            ), 'Seoul', 'other'
+                        ), 2025, 9600000, 2537300000),
+                     RegionCondition(
+                         Region(
+                             GlobeRect(
+                                 24.55, 25.5, 54.75, 55.65
+                             ), 'Dubai', 'other'
+                         ), 2025, 4000000, 200000000
+                     ),
+                    RegionCondition(
+                        Region(
+                            GlobeRect(
+                                24, 26, -92, -88)
+                            , 'Gulf of Mexico', 'ocean'),
+                        2025, 0, 0),
+                    RegionCondition(
+                        Region(
+                            GlobeRect(
+                                35.15, 35.45, -120.80, -120.4)
+                            , 'SLO', 'forest'),
+                        2025, 130000, 1200000)
+]
