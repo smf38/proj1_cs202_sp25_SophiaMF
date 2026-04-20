67047d6b49c4acb7b6a9426b6975647b476e506c
smf38 <smitch38@calpoly.edu>
2026-04-20 10:00:13 -0700

    task 1 completed (dataclasses)

--- proj1.py
+++ proj1.py
@@ -1,2 +1,27 @@
 #complete your tasks in this file
 #I was here
+
+#Task 1
+from dataclasses import dataclass
+@dataclass
+class GlobeRect(frozen=True):
+    lo_lat: float
+    hi_lat: float
+    west_long: float
+    east_long: float
+
+@dataclass
+class Region(frozen=True):
+    rect: GlobeRect
+    name: str
+    terrain: str # ocean|mountains|forest|other
+
+@dataclass
+class RegionCondition(frozen=True):
+    region: Region
+    year: int
+    pop: int
+    ghg_rate: float
+
+#Task 2
+
